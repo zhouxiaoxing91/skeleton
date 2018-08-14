4b0e31c6bb3a5c4b1a21808423dba0c4e3f518ed
zhou.guangfeng <zhou.guangfeng@puscene.com>
2018-08-14 10:21:50 +0800

    整合 tk.mybatis

--- skeleton-model/src/main/java/com/nancy/entity/Book.java
+++ skeleton-model/src/main/java/com/nancy/entity/Book.java
@@ -1,32 +1,55 @@
 package com.nancy.entity;
 
+import javax.persistence.*;
+
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
 
     private String code;
 
+    /**
+     * @return id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * @param id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name
+     */
     public void setName(String name) {
         this.name = name == null ? null : name.trim();
     }
 
+    /**
+     * @return code
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * @param code
+     */
     public void setCode(String code) {
         this.code = code == null ? null : code.trim();
     }
