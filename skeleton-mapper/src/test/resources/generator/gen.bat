0e043e3c8bbf72891faa4117c1418def595fff67
zhou.guangfeng <zhou.guangfeng@puscene.com>
2018-08-14 16:34:34 +0800

    win版 gen.bat

--- skeleton-mapper/src/test/resources/generator/gen.bat
+++ skeleton-mapper/src/test/resources/generator/gen.bat
@@ -1,4 +1,4 @@
 set CLASSPATH=CLASSPATH=mysql-connector-java-5.1.35.jar:mapper-3.4.3.jar:mybatis-generator-core-1.3.2.jar
 set mainClassName=org.mybatis.generator.api.ShellRunner
-java -classpath "%CLASSPATH%" "%mainClassName%" -configfile generatorConfig_koubei.xml -overwrite
+java -classpath "%CLASSPATH%" "%mainClassName%" -configfile generatorConfig.xml -overwrite
 pause
